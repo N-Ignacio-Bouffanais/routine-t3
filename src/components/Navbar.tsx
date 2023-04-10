fb11916b0fcfdf745de5da566643b9407df97b71
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-04-10 18:26:10 -0400

    [feature] layout, navbar basic, footer

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+
+const Navbar = () => {
+    return (
+        <nav className="bg-black h-14 flex items-center">
+            <ul className="flex w-4/5 mx-auto justify-end">
+                <li>
+                    <button className="rounded-full bg-gray-600 mx-2 px-4 py-2 font-semibold text-white no-underline transition hover:bg-white/20">Sign Out</button>
+                </li>
+                <li><button className="rounded-full bg-gray-600 mx-2 px-4 py-2 font-semibold text-white no-underline transition hover:bg-white/20">Login</button></li>
+            </ul>
+        </nav>
+
+    )
+}
+
+export default Navbar

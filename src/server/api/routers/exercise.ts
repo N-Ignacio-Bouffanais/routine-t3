7db08e5fc90e986e9219bca18f548d9b6cfad792
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-04-09 21:55:23 -0400

    [feature] crud operations

--- src/server/api/routers/exercise.ts
+++ src/server/api/routers/exercise.ts
@@ -39,6 +39,50 @@ const postExercise = publicProcedure
     });
   });
 
+const getById = publicProcedure.input(z.object({
+    id: z.string()
+  })).query(async ({ctx, input})=> {
+    try {
+      const result = await ctx.prisma.exercise.findUnique({
+      where: {
+        id: input.id,
+      }
+    })
+      return result;
+    } catch (error) {
+      console.log(error)
+    }
+  })
+
+const updateExercise = publicProcedure.input(
+  z.object({
+    id: z.string(),
+    day: z.string().trim().toLowerCase().min(5).max(9),
+    nameEx: z.string().min(3),
+    reps: z.number().min(3).max(40),
+    weight: z.number().min(2).max(150),
+    sets: z.number().min(1).max(7),
+  })
+).mutation(async ({ctx, input}) => {
+  try {
+    const result = await ctx.prisma.exercise.updateMany({
+      where: {
+        id: input.id,
+      },
+      data: {
+        day: input.day,
+        nameEx: input.nameEx,
+        reps: input.reps,
+        weight: input.weight,
+        sets: input.sets,
+      }
+    })
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+})
+
 const deleteExercise = publicProcedure
   .input(z.object({
     id: z.string()
@@ -57,4 +101,6 @@ export const exerciseRouter = createTRPCRouter({
   get: getExercises,
   create: postExercise,
   delete: deleteExercise,
+  update: updateExercise,
+  getOne: getById,
 });

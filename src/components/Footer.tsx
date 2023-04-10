231afbcb14b0f4364b7fb780c8cc2839a8ea047e
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-04-10 18:59:15 -0400

    [feature] footer , react icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,14 @@
+import { AiFillHome } from 'react-icons/ai'
+
+
 const Footer = () => {
     return (
-        <footer>
-           <ul className="flex rounded bg-gray-500">
-            <li></li></ul> 
+        <footer className="bg-dark-blue pb-6">
+            <ul className="flex rounded-2xl bg-gray-600 h-16 w-4/5 mx-auto p-5 items-center">
+                <li>
+                    <AiFillHome className='text-white w-8 h-8'/>
+                </li>
+            </ul>
         </footer>
     )
 }

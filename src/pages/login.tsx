ae5afe74dcc2b6f23b4195348388050edf2725f1
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-04-11 20:36:53 -0400

    [feature] delete registerform component, I give it a try nextauth

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -8,7 +8,6 @@ const login = () => {
       </picture>
       <p className='text-gray-400 my-3 w-96 text-center text-sm'>Make your own workout routines and define how and when do it.</p>
       <button className="py-2.5 text-white bg-blue-600 rounded-full my-3 w-32 font-semibold hover:bg-blue-700">Join</button>
-      <button className="py-2.5 text-white bg-blue-600 rounded-full my-3 w-32 font-semibold hover:bg-blue-700">Sign In</button>
     </div>
   )
 }

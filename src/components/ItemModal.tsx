4623bd1512f54b916aadefc6272cf05ca89242e8
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-04-13 19:20:09 -0400

    [feature] item modal cancel, setmodalopen state

--- src/components/ItemModal.tsx
+++ src/components/ItemModal.tsx
@@ -1,8 +1,22 @@
-import { FC } from "react"
+import { Dispatch, FC, SetStateAction } from "react"
 
-const ItemModal = () => {
+interface ItemModalProps{
+  setmodalOpen: Dispatch<SetStateAction<boolean>>
+}
+
+const ItemModal: FC<ItemModalProps> = ({ setmodalOpen }) => {
   return (
-    <div>itemModal</div>
+    <div className="absolute inset-0 flex items-center justify-center bg-dark-blue">
+      <div className="space-y-5 p-3 w-4/5 flex flex-col mx-auto items-center">
+        <h3 className="text-slate-300 text-2xl font-semibold">How you gonna do?</h3>
+        <input className="outline-none flex rounded-md h-9 w-80 px-4 font-medium" autoFocus type="text" placeholder="Name Exercise"/>
+        <input className="outline-none flex rounded-md h-9 w-80 px-4 font-medium" type="number" placeholder="Reps" />
+        <input className="outline-none flex rounded-md h-9 w-80 px-4 font-medium" type="number" placeholder="Weight" />
+        <input className="outline-none flex rounded-md h-9 w-80 px-4 font-medium" type="number" placeholder="Sets" />
+        <button className="h-9 w-80 rounded-md bg-sky-600 text-slate-50 font-semibold">Done</button>
+        <button onClick={() => setmodalOpen(false)} className="h-9 w-80 rounded-md bg-pink-700 text-slate-50 font-semibold">Cancel</button>
+      </div>
+    </div>
   )
 }
 
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,27 +1,33 @@
+import { NextPage } from 'next';
 import { getSession } from 'next-auth/react'
+import { useState } from 'react';
+import ItemModal from '~/components/ItemModal';
 import { api } from "~/utils/api";
 import exercises_images from '~/utils/exercises_img';
 
-const Home = () => {
-  const routines = api.exercise.get.useQuery();
-  console.log(routines.data)
+const Home: NextPage = () => {
+  const [modalOpen, setmodalOpen] = useState<boolean>(false)
+
+  //const routines = api.exercise.get.useQuery();
 
   return (
-    <div className='flex flex-col items-center sm:mx-auto bg-dark-blue sm:flex-wrap'>
-      <div className="py-5">
-        <button onClick={Select} className="rounded-full bg-green-400 px-4 py-2 font-semibold text-black no-underline transition hover:bg-white/20">New routine</button>
+    <>{modalOpen && <ItemModal setmodalOpen={setmodalOpen} />}
+      <div className='flex flex-col items-center bg-dark-blue sm:mx-auto sm:flex-wrap'>
+        <div className="py-5 flex justify-center w-4/5">
+          <div className='grid bg-gray-600 h-96 rounded-2xl w-40'>
+            <p>Routine:</p>
+          </div>
+          <button onClick={() => setmodalOpen(true)} className="rounded-2xl bg-blue-500 px-2 w-40 py-2 font-semibold text-white no-underline transition hover:bg-white/20">New routine</button>
+        </div>
       </div>
-    </div>
+    </>
   )
-}
-const Select = () => {
-  console.log(exercises_images[2])
 }
 
 export async function getServerSideProps(context: any) {
   const session = await getSession(context)
-  if(!session){
-    return{
+  if (!session) {
+    return {
       redirect: {
         destination: '/',
         permanent: false,

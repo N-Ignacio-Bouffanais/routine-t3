c7798c3da99ed98ed7f35239ee9c63e8d33b6c94
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-04-22 20:05:21 -0400

    [hotfix] changes to image from 200x200 to 144x144 and buttons scroll to 156

--- src/components/exercices.tsx
+++ src/components/exercices.tsx
@@ -3,12 +3,12 @@ import exercises_images from '~/utils/exercises_img';
 const Exercices = () => {
   const slideLeft = () => {
     const slider = document.getElementById('slider') as HTMLInputElement;
-    slider.scrollLeft = slider.scrollLeft - 204;
+    slider.scrollLeft = slider.scrollLeft - 156;
   };
 
   const slideRight = () => {
     const slider = document.getElementById('slider') as HTMLInputElement;
-    slider.scrollLeft = slider.scrollLeft + 204;
+    slider.scrollLeft = slider.scrollLeft + 156;
   };
   return (
     <div className='w-4/5 my-12'>
@@ -17,8 +17,8 @@ const Exercices = () => {
         <div className='flex gap-4 overflow-hidden flex-nowrap scroll-smooth' id='slider'>
           {exercises_images.map<JSX.Element>((image: { id: number, src: string, alt: string, name_exercise: string }) => (
             <div className='flex flex-col items-center' key={image.id}>
-              <picture className='flex w-48'>
-                <img className='flex-none  rounded-lg ' src={image.src} alt={image.alt} width={200} height={200} />
+              <picture className='flex w-36'>
+                <img className='flex-none  rounded-lg ' src={image.src} alt={image.alt} width={144} height={144} />
               </picture>
               <p className='text-white font-medium py-3'>{image.name_exercise}</p>
             </div>

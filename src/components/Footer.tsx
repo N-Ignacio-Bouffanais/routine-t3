ea4200e397c75079bf7d0e94bafa0f0f88a466c4
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-04-24 23:49:53 -0400

    [hotfix] footer render only when there are session data

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,23 @@
 import { AiFillHome } from 'react-icons/ai'
 import { BsFillPersonFill } from 'react-icons/bs'
+import { useSession } from "next-auth/react";
 
 //pt-10 md:pt-20
 const Footer = () => {
+    const { data: sessionData } = useSession();
     return (
-        <footer className="bg-dark-blue w-full ">
-            <ul className="flex bg-gray-blue h-20 mx-auto pt-2 pb-4 items-center justify-center">
-                <li className='px-5'>
-                    <AiFillHome className='text-white w-10 h-10'/>
-                </li>
-                <li className='px-5'>
-                    <BsFillPersonFill className='text-white w-10 h-10' />
-                </li>
-            </ul>
-        </footer>
+        <>
+            {sessionData && <footer className="bg-dark-blue w-full ">
+                <ul className="flex bg-gray-blue h-20 mx-auto pt-2 pb-4 items-center justify-center">
+                    <li className='px-5'>
+                        <AiFillHome className='text-white w-10 h-10' />
+                    </li>
+                    <li className='px-5'>
+                        <BsFillPersonFill className='text-white w-10 h-10' />
+                    </li>
+                </ul>
+            </footer>}
+        </>
     )
 }
 

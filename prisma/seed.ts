b7554c80086622bf390d95c053ed6001d686b848
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-04-26 23:05:53 -0400

    [hotfix] seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,41 +1,33 @@
 import { PrismaClient, Prisma } from "@prisma/client";
-
 const prisma = new PrismaClient();
-
-const userData: Prisma.UserCreateInput[] = [
-  {
-    name: "Alice",
-    email: "alice@prisma.io",
-    exercises: {
-      create: [
-        {
-          reps: 2,
-          day: "",
-          nameEx: "",
-          weight: 20,
-          sets: 4,
-        },
-      ],
+async function main() {
+  
+  const nico = await prisma.user.upsert({
+    where: { email: "bob@prisma.io" },
+    update: {},
+    create: {
+      email: "bob@prisma.io",
+      name: "Bob",
+      routines: {
+        create: [
+          {
+            routineName: "Full-Body",
+            day: "Lunes",
+          },
+        ],
+      },
     },
-  },
-];
-
-export async function main() {
-  try {
-    console.log(`Start seeding ...`)
-    for (const u of userData) {
-      const user = await prisma.user.create({
-        data: u,
-      })
-      console.log(`Created user with id: ${user.id}`)
-    }
-    console.log(`Seeding finished.`)
-  } catch (err) {
-    console.error(err)
-    process.exit(1)
-  } finally {
-    await prisma.$disconnect()
-  }
+  });
+  
+  
+  console.log({ nico });
 }
-
 main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
